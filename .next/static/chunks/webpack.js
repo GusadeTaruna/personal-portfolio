54d904232a2c8e7b17e8d1eb4f3b202c301c444e
GusadeTaruna <gusadetaruna04@gmail.com>
2022-01-03 07:01:48 +0800

    added bio

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e7b710c21a992a1"; }
+/******/ 		__webpack_require__.h = function() { return "c4fd813735544d31"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
-import { Container, Box, Heading, Image, useColorModeValue } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  Link,
+  useColorModeValue,
+  Button
+} from '@chakra-ui/react'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+import { ChevronRightIcon } from '@chakra-ui/icons'
 
 const Page = () => {
   return (
@@ -39,6 +51,31 @@ const Page = () => {
           />
         </Box>
       </Box>
+
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Gusade is a freelance developer who enjoys software development and
+          writing code, passioned about maintaining quality, secure and
+          performace optimized code, and also love to learn and explore about
+          new things or technology. He was graduated from Udayana University on
+          December 2021 with a bachelor's in engineering (Information Technology
+          major) and GPA 3.75. His most recent project is a website for{' '}
+          <NextLink href="/works/equatorial">
+            <Link>Equatorial Property</Link>
+          </NextLink>
+          .
+        </Paragraph>
+
+        <Box align='center' my={5}>
+          <NextLink href='/works'>
+            <Button rightIcon={<ChevronRightIcon />}colorScheme='linkedin'>My Portfolio</Button>
+          </NextLink>
+        </Box>
+
+      </Section>
     </Container>
   )
 }
